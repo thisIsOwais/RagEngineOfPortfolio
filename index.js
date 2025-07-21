07a18aedc750deb0ec66a6988eff7b1b110f2e4c
18owais <owaismohd857@gmail.com>
2025-07-21 21:07:23 +0530

    rename app.js to index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 import express from 'express'
 import cors from "cors";
 import { initRag, getRagResponse } from "./ragEngine.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -37,7 +39,24 @@ app.post("/api/ask", async (req, res) => {
   });
   
   
-
+  app.post("/api/onboard", (req, res) => {
+    const { key } = req.body;
+    console.log("Received key:", key); //
+    if (!key) {
+      return res.status(400).send("Key is required");
+    }
+    
+  
+    const isVerified = key === process.env.ONBOARDING_KEY;
+  
+    if (isVerified) {
+      return res.status(200).json({ success: true });
+    } else {
+      console.log("❌ Invalid key received"); //
+      return res.status(401).json({ success: false, message: "Invalid key" });
+    }
+  });
+  
 // Start server after RAG setup
 initRag().then( () => {
     app.listen(port, () => {
